5f59717cc6407cbcb9c6de1eec1ffc15957e7db1
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-09-13 17:46:07 -0300

    Pasando Opciones al Hook

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas =[
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'USD', nombre: 'Peso Mexicano'},
+    {id: 'USD', nombre: 'Euro'},
+    {id: 'USD', nombre: 'Libra Esterlina'},
+]
+
+export {
+
+    monedas
+}
