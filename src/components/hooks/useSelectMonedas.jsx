49434b9b5c68fff8ed02aedb78eba7ca3cd6cc26
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-09-13 10:19:58 -0300

    Creando un Custom Hook

---
+++ src/components/hooks/useSelectMonedas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+  
+    const SeletMonedas= () =>{
+        console.log('Desde Select Monedas')
+    }
+
+    return [SeletMonedas]
+}
+
+export default useSelectMonedas
